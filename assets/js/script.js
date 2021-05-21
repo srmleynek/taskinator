9ab0bf8b026e3595424d80aa7942ea5d34622728
SidMle <srmleynek@gmail.com>
2021-05-20 21:06:46 -0600

    finished 3.4.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 var taskFormHandler = function (event) {
     event.preventDefault();
@@ -24,19 +25,21 @@ var taskFormHandler = function (event) {
 };
 
 var createTaskEl = function(taskDataObj) {
-    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-
-    // create div to hold task info and add to list item
+  
+    // add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", taskIdCounter);
+  
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
     listItemEl.appendChild(taskInfoEl);
-
-    // add entire list item to list
+  
     tasksToDoEl.appendChild(listItemEl);
-}
+  
+    // increase task counter for next unique id
+    taskIdCounter++;
+  };
 
 formEl.addEventListener("submit", taskFormHandler);
