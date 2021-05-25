f3ff307f9b8214f164929c49d7e4fec1cad014e9
SidMle <srmleynek@gmail.com>
2021-05-25 17:07:24 -0600

    load function

--- assets/js/script.js
+++ assets/js/script.js
@@ -213,6 +213,47 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+var loadTasks = function() {
+    var savedTasks = localStorage.getItem("tasks");
+  
+    if (savedTasks === null) {
+      return false;
+    }
+  
+    savedTasks = JSON.parse(savedTasks);
+  
+    for (var i = 0; i < savedTasks.length; i++) {
+      // pass each task object into the `createTaskEl()` function
+      createTaskEl(savedTasks[taskIdCounter]);
+  
+      var listItemEl = document.createElement("li");
+      listItemEl.className = "task-item";
+      listItemEl.setAttribute("data-task-id", tasks[i].id);
+      listItemEl.setAttribute("draggable", "true");
+  
+      var taskInfoEl = document.createElement("div");
+      taskInfoEl.className = "task-info";
+      taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+      listItemEl.appendChild(taskInfoEl);
+  
+      var taskActionsEl = createTaskActions(tasks[i].id);
+      listItemEl.appendChild(taskActionsEl);
+    
+      if (tasks[i].status === "to do") {
+        listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+        tasksToDoEl.appendChild(listItemEl);
+       }
+       else if (tasks[i].status === "in progress") {
+        listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+        tasksInProgressEl.appendChild(listItemEl)
+       }
+       else if (tasks[i].status === "completed") {
+        listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+        tasksCompletedEl.appendChild(listItemEl)
+       }
+      }  
+    };
+
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
